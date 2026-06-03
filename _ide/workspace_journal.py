d50b7e26848b933a23e5d0f9347064661a44d892
rt7085 <remcoterwal@rcn.com>
2026-06-03 07:01:01 -0400

    Updating git after opening vivdo/vitis

--- _ide/workspace_journal.py
+++ _ide/workspace_journal.py
@@ -1,82 +1,8 @@
-# 2026-06-01T06:10:03.937231690
+# 2026-06-03T06:59:24.971136786
 import vitis
 
 client = vitis.create_client()
 client.set_workspace(path="tagus_led_blinker")
-
-platform = client.get_component(name="tagus_led_blinker_platform")
-status = platform.build()
-
-comp = client.get_component(name="hello_world")
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
 
 vitis.dispose()
 
---
+++ _ide/workspace_journal_257839.py
@@ -0,0 +1,82 @@
+# 2026-06-01T06:10:03.937231690
+import vitis
+
+client = vitis.create_client()
+client.set_workspace(path="tagus_led_blinker")
+
+platform = client.get_component(name="tagus_led_blinker_platform")
+status = platform.build()
+
+comp = client.get_component(name="hello_world")
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+vitis.dispose()
+
